a5184827df2608c07bb410a838ac5444483fe78a
daijichennai <daijiworldtech@gmail.com>
2019-08-02 14:47:34 +0530

    Sidemenu issue

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor() {
+  constructor(public menuCtrl: MenuController) {
+  }
+  ionViewWillEnter() {
+    this.menuCtrl.enable(true);
   }
 }
